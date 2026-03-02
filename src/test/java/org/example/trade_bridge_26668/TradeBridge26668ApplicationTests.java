afc147f9f1c66ae084c55f3dc177592a24ad9df6
NOBLE <norbertndikumana@gmail.com>
2026-03-02 15:16:04 +0200

    test: Add test configuration

---
+++ src/test/java/org/example/trade_bridge_26668/TradeBridge26668ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.trade_bridge_26668;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBridge26668ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
