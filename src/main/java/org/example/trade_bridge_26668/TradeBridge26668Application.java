5c1066c4fe2248d8cbd3fe915b320541161393ad
NOBLE <norbertndikumana@gmail.com>
2026-03-02 15:11:45 +0200

    feat: Add main Spring Boot application class

---
+++ src/main/java/org/example/trade_bridge_26668/TradeBridge26668Application.java
@@ -0,0 +1,11 @@
+package org.example.trade_bridge_26668;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBridge26668Application {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeBridge26668Application.class, args);
+    }
+}
